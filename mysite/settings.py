c7f3a261f8d60380c5536c86cecec4068ddb51e3
Siddharth1896 <89393140+Siddharth1896@users.noreply.github.com>
2021-08-24 12:57:28 +0200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    #'import_export',
+    'import_export',
     'myapp.apps.MyappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
