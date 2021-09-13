4508c574831f76a297a8bea1176acbfca298ca25
Siddharth1896 <89393140+Siddharth1896@users.noreply.github.com>
2021-09-13 19:23:32 +0200

    Update admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 #from django import import_export
 from import_export.admin import ImportExportActionModelAdmin
 from django_admin_listfilter_dropdown.filters import DropdownFilter
-from advanced_filters.admin import AdminAdvancedFiltersMixin
+#from advanced_filters.admin import AdminAdvancedFiltersMixin
 
 # Register your models here.
 
 from .models import Data
 
 @admin.register(Data)
-class ViewAdmin(AdminAdvancedFiltersMixin, ImportExportActionModelAdmin):
+class ViewAdmin(ImportExportActionModelAdmin):
     list_display = ('License_name','Part_number','Date_of_entry','Country','Vendor','Metric','Currency','Process_number','GLobal_price_listprice_USD','Discount_from_pricelist','Final_price_USD','Finalprice_of_Localcurrency','Awarded','Quantity','Volume_of_Deal')
     list_per_page = 50
     list_filter = (
